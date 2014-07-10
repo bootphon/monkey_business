17eaeee4307c5f2dabce36de0473a9ca98856d8a
Maarten Versteegh <maartenversteegh@gmail.com>
2014-07-10 21:51:53 +0200

    add voice activity detector (mad.py) and script to run over monkey files

---
+++ do_mad.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------
+# file: do_mad.py
+# date: Thu July 10 01:49 2014
+# author:
+# Maarten Versteegh
+# github.com/mwv
+# maartenversteegh AT gmail DOT com
+#
+# Licensed under GPLv3
+# ------------------------------------
+"""do_mad:
+
+"""
+
+from __future__ import division
+
+from __future__ import division
+import scikits.audiolab as audiolab
+import os
+import os.path as path
+import fnmatch
+import cPickle as pickle
+
+from joblib import delayed, Parallel
+
+import mad
+
+def rglob(rootdir, pattern):
+    for root, _, files in os.walk(rootdir):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                yield path.join(root, basename)
+
+
+def get_Lambda_single(wavfile, vad):
+    sig, fs, _ = audiolab.wavread(wavfile)
+    return path.splitext(path.basename(wavfile))[0], vad.calc_Lambda(sig, fs)
+
+
+def pred_Lambdas(monkey, n_cores=20):
+    wavfiles = sorted(list(rglob(path.join(BASEDIR, monkey), '*.wav')))
+
+    p = Parallel(n_jobs=n_cores, verbose=11)(delayed(get_Lambda_single)
+                                             (w, mad.MAD(epsilon=1e-4,
+                                                         NFFT=512))
+                                             for w in wavfiles)
+    return dict(p)
+
+BASEDIR = path.join(os.environ['HOME'], 'data', 'monkey_sounds')
+# MONKEYS = ['Titi_monkeys', 'Blue_monkeys', 'colobus', 'gibbons']
+MONKEYS = ['Titi_monkeys', 'Blue_monkeys']
+NCORES = 20
+
+if __name__ == '__main__':
+    for monkey in MONKEYS:
+        print monkey
+        pred_lambdas = pred_Lambdas(monkey, n_cores=NCORES)
+        with open('pred_lambdas_{0}.pkl'.format(monkey), 'wb') as fid:
+            pickle.dump(pred_lambdas, fid, -1)
---
+++ mad.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------
+# file: mad.py
+# date: Fri July 04 13:30 2014
+# author:
+# Maarten Versteegh
+# github.com/mwv
+# maartenversteegh AT gmail DOT com
+#
+# Licensed under GPLv3
+# ------------------------------------
+"""mad: Monkey Activity Detector
+
+"""
+
+from __future__ import division
+
+import numpy as np
+from scipy.fftpack import fft
+from scipy.special import jn
+
+class MAD(object):
+    """
+    """
+
+    def __init__(self, markov_params=(0.5, 0.1), alpha=0.99, NFFT=1024,
+                 n_iters=10, win_size_sec=0.05, win_hop_sec=0.025,
+                 max_est_iter=-1, epsilon=1e-6):
+        """
+
+        Arguments:
+        :param markov_params: hangover scheme params
+        :param alpha: SNR estimate coefficient
+        :param NFFT: size of FFT
+        :param n_iters: number of iterations in noise estimation
+        :param win_size_sec: window size in seconds
+        :param win_hop_sec: hop size in seconds
+        :param epsilon: convergence epsilon
+        """
+        self.a01, self.a10 = markov_params
+        self.a00 = 1 - self.a01
+        self.a11 = 1 - self.a10
+        self.alpha = alpha
+        self.NFFT = NFFT
+        self.win_size_sec = win_size_sec
+        self.win_hop_sec = win_hop_sec
+        self.epsilon = epsilon
+        self.n_iters = n_iters
+        self.max_est_iter = max_est_iter
+
+    def detect_speech(self, sig, fs, threshold, n_noise_frames=20):
+        return self.calc_Lambda(sig, fs, n_noise_frames) > threshold
+
+    def calc_Lambda(self, sig, fs, n_noise_frames=20):
+        win_size_smp = int(fs * self.win_size_sec)
+        win_hop_smp = int(fs * self.win_hop_sec)
+        sig_length = len(sig)
+        hamming_win = np.hamming(win_size_smp)
+        n_frames = int(np.floor((sig_length - win_size_smp) / win_hop_smp))
+
+        noise_var_tmp = np.zeros(self.NFFT)
+        for n in range(n_noise_frames):
+            frame = fft(hamming_win * sig[n * win_hop_smp:
+                                          n * win_hop_smp + win_size_smp],
+                        self.NFFT)
+            noise_var_tmp = noise_var_tmp + (np.conj(frame) * frame).real
+
+        noise_var_orig = noise_var_tmp / n_noise_frames
+        noise_var_old = noise_var_orig
+
+        G_old = 1
+        A_MMSE = np.zeros((self.NFFT, n_frames))
+        G_MMSE = np.zeros((self.NFFT, n_frames))
+
+        cum_Lambda = np.zeros(n_frames)
+        for n in xrange(n_frames):
+            frame = fft(hamming_win * sig[n * win_hop_smp:
+                                          n * win_hop_smp + win_size_smp],
+                        self.NFFT)
+            frame_var = (np.conj(frame) * frame).real
+
+            noise_var = noise_var_orig
+
+            # do noise estimation
+            if self.max_est_iter == -1 or n < self.max_est_iter:
+                noise_var_prev = noise_var_orig
+                for _ in xrange(self.n_iters):
+                    gamma = frame_var / noise_var
+                    Y_mag = np.abs(frame)
+
+                    if n:
+                        xi = (self.alpha *
+                              ((A_MMSE[:, n-1]**2 / noise_var_old) +
+                               (1 - self.alpha) * np.maximum(gamma - 1, 0)))
+                    else:
+                        xi = (self.alpha +
+                              (1 - self.alpha) * np.maximum(gamma - 1, 0))
+
+                    v = xi * gamma / (1 + xi)
+                    G_MMSE[:, n] = (np.sqrt(np.pi) / 2) * (np.sqrt(v) / gamma) * \
+                                   np.exp(v/-2) * ((1 + v) * jn(0, v/2) +
+                                                   v * jn(1, v/2))
+                    G_MMSE[np.isnan(G_MMSE)] = 1
+                    G_MMSE[np.isinf(G_MMSE)] = 1
+
+                    A_MMSE[:, n] = G_MMSE[:, n] * Y_mag
+
+                    gamma_term = gamma * xi / (1 + xi)
+                    gamma_term = np.minimum(gamma_term, 1e-2)
+                    Lambda_mean = (1 / (1 + xi) + np.exp(gamma_term)).mean()
+
+                    weight = Lambda_mean / (1 + Lambda_mean)
+                    if np.isnan(weight):
+                        weight = 1
+
+                    noise_var = weight * noise_var_orig + (1 - weight) * frame_var
+
+                    diff = np.abs(np.sum(noise_var - noise_var_prev))
+                    if diff < self.epsilon:
+                        break
+                    noise_var_prev = noise_var
+
+            gamma = frame_var / noise_var
+            Y_mag = np.abs(frame)
+
+            if n:
+                xi = self.alpha * ((A_MMSE[:, n-1]**2 / noise_var_old) +
+                                   (1 - self.alpha) * np.maximum(gamma - 1, 0))
+            else:
+                xi = self.alpha + (1 - self.alpha) * np.maximum(gamma - 1, 0)
+
+            v = (xi * gamma) / (1 + xi)
+            G_MMSE[:, n] = (np.sqrt(np.pi) / 2) * (np.sqrt(v) / gamma) * \
+                np.exp(v/-2) * ((1 + v) * jn(0, v/2) + v * jn(1, v/2))
+            G_MMSE[np.isnan(G_MMSE)] = 1
+            G_MMSE[np.isinf(G_MMSE)] = 1
+
+            A_MMSE[:, n] = G_MMSE[:, n] * Y_mag
+
+            Lambda_mean = (np.log(1/(1+xi)) + gamma * xi / (1 + xi)).mean()
+
+            G = ((self.a01 + self.a11 * G_old) /
+                 (self.a00 + self.a10 * G_old) * Lambda_mean)
+
+            cum_Lambda[n] = G
+
+            G_old = G
+            noise_var_old = noise_var
+        return cum_Lambda
