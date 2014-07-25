619e1917aabe8e313c1f7d7f20ac1eb23e8f148f
syhw <gabriel.synnaeve@gmail.com>
2014-07-25 13:59:02 +0200

    clusterize + deep nets

--- cluster.py
+++ cluster.py
@@ -1,25 +1,45 @@
 import numpy as np
 from sklearn import mixture, cluster, metrics
-from extract_data import load_from_npz
 
-fname = "blue_monkeys.npz"
+from mielke_replication import load_all_monkeys, combine_labels
+MONKEYS = ['Titi_monkeys', 'Blue_monkeys', 'colobus']
 
-fbanks, labels, strided, labels_strided, stride, labels_set = load_from_npz(
-        fname)
+X, y, labelset = load_all_monkeys()
+print labelset
+
+clusterizers = [{
+    'Titi_monkeys': cluster.KMeans(init='k-means++', n_clusters=
+        len(labelset['Titi_monkeys']) + 1, n_init=10),
+    'Blue_monkeys': cluster.KMeans(init='k-means++', n_clusters=
+        len(labelset['Blue_monkeys']) + 1, n_init=10),
+    'colobus': cluster.KMeans(init='k-means++', n_clusters=
+        len(labelset['colobus']) + 1, n_init=10),
+    'combined': cluster.KMeans(init='k-means++', n_clusters=7, n_init=10)}, {
+    'Titi_monkeys': mixture.VBGMM(n_components=
+        len(labelset['Titi_monkeys']) + 1, covariance_type='diag'),
+    'Blue_monkeys': mixture.VBGMM(n_components=
+        len(labelset['Blue_monkeys']) + 1, covariance_type='diag'),
+    'colobus': mixture.VBGMM(n_components=
+        len(labelset['colobus']) + 1, covariance_type='diag'),
+    'combined': mixture.VBGMM(n_components=7, covariance_type='diag')}]
+
+for clusterizer in clusterizers:
+    # 1. classify calls per monkey
+    for monkey in MONKEYS:
+        print monkey
+        clusterizer[monkey].fit(X[monkey])
+        print X[monkey].shape
+        y_pred = clusterizer[monkey].predict(X[monkey])
+        print metrics.adjusted_rand_score(y_pred, y[monkey])
+
+    # 2. classify over all monkeys and calls
+    X_comb, y_comb, labelset_comb = combine_labels(X, y, labelset)
+    clusterizer['combined'].fit(X_comb)
+    y_pred = clusterizer['combined'].predict(X_comb)
+    print 'COMBINED:'
+    print metrics.adjusted_rand_score(y_pred, y_comb)
 
 
-kmeans = cluster.KMeans(init='k-means++', n_clusters=2, # len(labels_set)+1
-        n_init=10)
-labels_pred = kmeans.fit_predict(strided)
-print kmeans
-print metrics.adjusted_rand_score(labels_pred, labels_strided)
-
-dpgmm = mixture.VBGMM(n_components=2, # len(labels_set)+1
-        covariance_type='diag')
-dpgmm.fit(strided)
-labels_pred = dpgmm.predict(strided)
-print dpgmm
-print metrics.adjusted_rand_score(labels_pred, labels_strided)
 
 # TODO check / eval
 # TODO spectral clustering
---
+++ deep.py
@@ -0,0 +1,89 @@
+import numpy as np
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+from nnet import DropoutNet, RegularizedNet, add_fit_score_predict
+from nnet import ReLU, LogisticRegression
+
+from mielke_replication import load_all_monkeys, combine_labels, make_monkey_set
+MONKEYS = ['Titi_monkeys', 'Blue_monkeys', 'colobus']
+
+def train_deep():
+    add_fit_score_predict(DropoutNet)
+    add_fit_score_predict(RegularizedNet)
+
+    X, y, labelset = load_all_monkeys()
+    # 1. classify calls per monkey
+    for monkey in MONKEYS:
+        print monkey
+        X_train, X_test, y_train, y_test = train_test_split(X[monkey],
+                                                            y[monkey],
+                                                            test_size=0.2)
+        X_train = np.asarray(X_train, dtype='float32')
+        X_test = np.asarray(X_test, dtype='float32')
+        y_train = np.asarray(y_train, dtype='int32')
+        y_test = np.asarray(y_test, dtype='int32')
+        numpy_rng = np.random.RandomState(123)
+        clf = DropoutNet(numpy_rng=numpy_rng,
+                n_ins=X_train.shape[1],
+                layers_types=[ReLU, ReLU, ReLU, ReLU, LogisticRegression],
+                layers_sizes=[2000, 2000, 2000, 2000],
+                dropout_rates=[0., 0.5, 0.5, 0.5, 0.5],
+                n_outs=len(labelset[monkey]),
+                fast_drop=False,
+                debugprint=0)
+
+        clf.fit(X_train, y_train, max_epochs=200, verbose=True)
+        #clf.fit(x_train, y_train, max_epochs=n_epochs, method=method, verbose=VERBOSE, plot=PLOT)
+        y_pred = clf.predict(X_test)
+        print metrics.classification_report(y_test, y_pred,
+                                            target_names=labelset[monkey])
+
+    # 2. classify over all monkeys and calls
+    X_comb, y_comb, labelset_comb = combine_labels(X, y, labelset)
+    X_train, X_test, y_train, y_test = train_test_split(X_comb, y_comb,
+                                                        test_size=0.2)
+
+    X_train = np.asarray(X_train, dtype='float32')
+    X_test = np.asarray(X_test, dtype='float32')
+    y_train = np.asarray(y_train, dtype='int32')
+    y_test = np.asarray(y_test, dtype='int32')
+    clf = DropoutNet(numpy_rng=numpy_rng,
+            n_ins=X_train.shape[1],
+            layers_types=[ReLU, ReLU, ReLU, ReLU, LogisticRegression],
+            layers_sizes=[2000, 2000, 2000, 2000],
+            dropout_rates=[0., 0.5, 0.5, 0.5, 0.5],
+            n_outs=len(labelset[monkey]),
+            fast_drop=False,
+            debugprint=0)
+    clf.fit(X_train, y_train, max_epochs=200, verbose=True)
+    y_pred = clf.predict(X_test)
+    print 'COMBINED:'
+    print metrics.classification_report(y_test, y_pred,
+                                        target_names=labelset_comb)
+
+    # 3. classify monkeys
+    X_comb, y_comb, labelset_comb = make_monkey_set(X, y, labelset)
+    X_train, X_test, y_train, y_test = train_test_split(X_comb, y_comb,
+                                                        test_size=0.2)
+    X_train = np.asarray(X_train, dtype='float32')
+    X_test = np.asarray(X_test, dtype='float32')
+    y_train = np.asarray(y_train, dtype='int32')
+    y_test = np.asarray(y_test, dtype='int32')
+    clf = DropoutNet(numpy_rng=numpy_rng,
+            n_ins=X_train.shape[1],
+            layers_types=[ReLU, ReLU, ReLU, ReLU, LogisticRegression],
+            layers_sizes=[2000, 2000, 2000, 2000],
+            dropout_rates=[0., 0.5, 0.5, 0.5, 0.5],
+            n_outs=len(labelset[monkey]),
+            fast_drop=False,
+            debugprint=0)
+    clf.fit(X_train, y_train, max_epochs=200, verbose=True)
+    y_pred = clf.predict(X_test)
+    print 'SPECIES:'
+    print metrics.classification_report(y_test, y_pred,
+                                        target_names=labelset_comb)
+
+
+if __name__ == '__main__':
+    train_deep()
+
